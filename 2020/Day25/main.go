8ccc3d444f8c6a1b258465becf877c326c5a8605
Alexander Pochill <noseglid@gmail.com>
2020-12-25 10:38:04 +0100

    2020: Day25

---
+++ 2020/Day25/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+)
+
+func findPrivateKey(sub, pub int) int {
+	key := 1
+	loopSize := 0
+	for key != pub {
+		key = key * sub
+		key = key % 20201227
+		loopSize++
+	}
+
+	return loopSize
+}
+
+func transform(sub, loopSize int) int {
+	v := 1
+	for i := 0; i < loopSize; i++ {
+		v = v * sub
+		v = v % 20201227
+	}
+
+	return v
+}
+
+func main() {
+	doorpub := 14082811
+	cardpub := 5249543
+
+	// doorpub := 17807724 // sample
+	// cardpub := 5764801  // sample
+
+	cardpriv := findPrivateKey(7, cardpub)
+	log.Printf("private card: %d", cardpriv)
+	encKey := transform(doorpub, cardpriv)
+
+	log.Printf("encryption key (part1): %d", encKey)
+
+}
