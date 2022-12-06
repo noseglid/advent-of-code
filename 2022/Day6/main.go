3cd0ca113209e9687ee5a55ef21d0f80f4517137
Alexander Pochill <noseglid@gmail.com>
2022-12-06 07:08:30 +0100

    2022: Day 6

---
+++ 2022/Day6/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/noseglid/advent-of-code/util"
+)
+
+func startOfX(l []rune) bool {
+	m := map[rune]struct{}{}
+	for _, r := range l {
+		if _, ok := m[r]; ok {
+			return false
+		}
+		m[r] = struct{}{}
+	}
+	return true
+}
+
+func main() {
+	// input := "nppdvjthqldpwncqszvftbrmjlhg"
+	input := util.GetFile("2022/Day6/input")
+
+	p, m := false, false
+	for i := 3; i < len(input); i++ {
+		if !p && startOfX([]rune(input[util.MaxInt(0, i-4+1):i+1])) {
+			fmt.Printf("start-of-packet (part1): %d\n", i+1)
+			p = true
+		}
+		if !m && startOfX([]rune(input[util.MaxInt(0, i-14+1):i+1])) {
+			fmt.Printf("start-of-message (part2): %d\n", i+1)
+			m = true
+		}
+		if m && p {
+			break
+		}
+	}
+
+}
