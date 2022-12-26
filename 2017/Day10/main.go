082d4447fc11926e5e4dc37afad3b37b7cde409c
Alexander Pochill <noseglid@gmail.com>
2022-12-26 18:04:43 +0100

    2017: Day 10

--- 2017/Day10/main.go
+++ 2017/Day10/main.go
@@ -1,106 +1,98 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"strings"
 
 	"github.com/noseglid/advent-of-code/util"
 )
 
-func listString(l []int) string {
-	var sb strings.Builder
-	sep := ""
-	for _, v := range l {
-		sb.WriteString(fmt.Sprintf("%s%d", sep, v))
-		sep = " -> "
+func parseLengths(l string) []int {
+	var r []int
+	for _, n := range strings.Split(l, ",") {
+		r = append(r, util.MustAtoi(n))
 	}
-	return sb.String()
+
+	return r
 }
 
-func reverse(l []int) {
-	for i := 0; i < len(l)/2; i++ {
-		l[i], l[len(l)-i-1] = l[len(l)-i-1], l[i]
+func parseInput(l string) []int {
+	var r []int
+	for _, n := range l {
+		r = append(r, int(n))
+	}
+	return r
+
+}
+
+func copySublist(src []int, start, end int) []int {
+	if end == start {
+		return []int{src[end]}
+	} else if end > start {
+		ret := make([]int, end-start)
+		copy(ret, src[start:end])
+		return ret
+	} else {
+		ret := make([]int, len(src)-start+end)
+		copy(ret, src[start:])
+		copy(ret[len(src)-start:], src[:start])
+		return ret
+	}
+}
+
+func makeList(size int) []int {
+	list := make([]int, size)
+	for i := 0; i < size; i++ {
+		list[i] = i
+	}
+	return list
+}
+
+func iterate(lengths []int, list []int, iterations int) {
+	skipSize := 0
+	position := 0
+	for i := 0; i < iterations; i++ {
+		for _, e := range lengths {
+			start, end := position, (position+e)%len(list)
+			sublist := copySublist(list, start, end)
+			util.Reverse(sublist)
+
+			j := 0
+			for i := start; i != end; i = (i + 1) % len(list) {
+				list[i] = sublist[j]
+				j++
+			}
+
+			position = (position + e + skipSize) % len(list)
+			skipSize++
+		}
 	}
 }
 
 func main() {
-	// input := "206,63,255,131,65,80,238,157,254,24,133,2,16,0,1,3"
-	// size := 256
-	input := "3, 4, 1, 5"
-	size := 5
+	input := "206,63,255,131,65,80,238,157,254,24,133,2,16,0,1,3"
+	size := 256
 
-	var lengths []int
-	for _, s := range strings.Split(input, ",") {
-		lengths = append(lengths, util.MustAtoi(strings.TrimSpace(s)))
+	lengths := parseLengths(input)
+	list := makeList(size)
+
+	iterate(lengths, list, 1)
+	fmt.Printf("Multiply first two numbers (part1): %d\n", list[0]*list[1])
+
+	lengthsp2 := append(parseInput(input), []int{17, 31, 73, 47, 23}...)
+	listp2 := makeList(256)
+	iterate(lengthsp2, listp2, 64)
+
+	xord := make([]byte, 16)
+	for i := 0; i < len(listp2); i += 16 {
+		n := listp2[i]
+		for j := 1; j < 16; j++ {
+			n ^= listp2[i+j]
+		}
+		xord[i/16] = byte(n)
+
 	}
-	currentPosition := 0
-	skipSize := 0
-	_ = skipSize
-
-	list := make([]int, size)
-	for i := range list {
-		list[i] = i
-	}
-
-	for _, ll := range lengths {
-		fmt.Println("=== ROUND START ===")
-		fmt.Printf("currentPosition=%d, skipSize=%d, length=%d\n", currentPosition, skipSize, ll)
-		fmt.Printf(">> %s\n", listString(list))
-		newList := append([]int{}, list[currentPosition:]...)
-		newList = append(newList, list[:currentPosition]...)
-		fmt.Printf("rotated list >> %s\n", listString(newList))
-		reverse(newList[:ll])
-		fmt.Printf("reversed and rotated list >> %s\n", listString(newList))
-		list = append([]int{}, newList[currentPosition+1:]...)
-		list = append(list, newList[:currentPosition+1]...)
-		fmt.Printf("list >> %s\n", listString(list))
-
-		currentPosition = (currentPosition + ll + skipSize) % len(list)
-		skipSize++
-
-		fmt.Printf("=== ROUND END ===\n\n")
-	}
-
-	fmt.Printf("Final >> %s\n", listString(list))
-
-	// list := util.NewCircularLinkedList[int]()
-	// for i := 0; i < size; i++ {
-	// 	list.Add(i)
-	// }
-
-	// // // cc := 3
-	// for _, ll := range lengths {
-	// 	fmt.Print("=== ROUND START ===\n")
-	// 	fmt.Printf("list=%s\n", list.String())
-	// 	fmt.Printf("length=%d, currentPosition=%d, skipSize=%d\n", ll, currentPosition, skipSize)
-	// 	from := currentPosition
-	// 	to := (currentPosition + ll - 1) % list.Len()
-	// 	fmt.Printf("from=%d, to=%d\n", from, to)
-
-	// 	list.Reverse(from, to)
-	// 	fmt.Printf("reversed %s\n", list.String())
-
-	// 	currentPosition = (currentPosition + ll + skipSize) % list.Len()
-	// 	skipSize++
-	// 	fmt.Printf("=== ROUND END ===\n\n")
-
-	// 	// 	fmt.Printf("reversed\n")
-	// 	// 	fmt.Printf("using length: %d\n", ll)
-	// 	// 	ptr.reverse(ll - 1)
-	// 	// 	head.print()
-	// 	// 	fmt.Printf("---\n")
-
-	// 	// 	currentPosition += lengths[0] + skipSize
-	// 	// 	lengths = lengths[1:]
-	// 	// 	skipSize++
-	// 	// 	fmt.Printf("currentPosition=%d, skipSize=%d\n", currentPosition, skipSize)
-
-	// 	// 	// cc--
-	// 	// 	// if cc == 0 {
-	// 	// 	// 	break
-	// 	// 	// }
-	// }
-
-	fmt.Printf("Part 1: Multiplied numbers: %d", list[0]*list[1])
+	fmt.Printf("Hash of '%s' (part2): %s\n", input, hex.EncodeToString(xord))
 
 }
